d53a9e9e03b7e1af081e1710cd18e0566207f342
jacegonzales25 <jacegonzales25@gmail.com>
2024-09-29 15:26:37 +0800

    feat: add relations and foreign key references

--- server/db/schema.ts
+++ server/db/schema.ts
@@ -5,40 +5,43 @@ import {
   timestamp,
   uniqueIndex,
   integer,
-  boolean
-} from 'drizzle-orm/pg-core';
+  boolean,
+} from "drizzle-orm/pg-core";
+import { relations } from "drizzle-orm";
 
+export const users = pgTable(
+  "users",
+  {
+    id: serial("id").primaryKey(),
+    email: text("email").notNull().unique(),
+    createdAt: timestamp("createdAt").defaultNow().notNull(),
+  },
+  (users) => {
+    return {
+      uniqueIdx: uniqueIndex("unique_idx").on(users.email),
+    };
+  }
+);
 
-export const usersTable = pgTable(
-    'users',
-    {
-        id: serial('id').primaryKey(),
-        email: text('email').notNull().unique(),
-        createdAt: timestamp('createdAt').defaultNow().notNull(),
-    }, (users) => {
-        return {
-            uniqueIdx: uniqueIndex('unique_idx').on(users.email)
-        }
-    }
-)
+export const photos = pgTable("photos", {
+  id: serial("id").primaryKey(),
+  userId: integer("user_id"),
+  url: text("url").notNull(),
+  createdAt: timestamp("createdAt").defaultNow().notNull(),
+});
 
-export const photosTable = pgTable(
-    'photos',
-    {
-        id: serial('id').primaryKey(),
-        userId: integer("user_id"),
-        url: text('url').notNull(),
-        createdAt: timestamp('createdAt').defaultNow().notNull(),
-    }
-)
+export const resumes = pgTable("resumes", {
+  id: serial("id").primaryKey(),
+  userId: integer("user_id").references(() => users.id),
+  content: text("content"),
+  isDraft: boolean("isDraft").default(false),
+  createdAt: timestamp("createdAt").defaultNow().notNull(),
+});
 
-export const resumesTable = pgTable(
-    'resumes',
-    {
-        id: serial('id').primaryKey(),
-        userId: integer("user_id"),
-        content: text('content'),
-        isDraft: boolean('isDraft').default(false),
-        createdAt: timestamp('createdAt').defaultNow().notNull(),
-    }
-)
+export const photosRelations = relations(users, ({ many }) => ({
+  photos: many(photos),
+}));
+
+export const resumesRelations = relations(users, ({one}) => ({
+    resumes: one(resumes)
+}))
