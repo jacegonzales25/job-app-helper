403cd1967d0319e2543c91b61b8e4764c1c144b4
jacegonzales25 <jacegonzales25@gmail.com>
2024-10-22 11:32:54 +0800

    feat: modularized education

---
+++ components/resume/education-form.tsx
@@ -0,0 +1,212 @@
+"use client";
+
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useFieldArray, useForm } from "react-hook-form";
+import { z } from "zod";
+import { Card, CardContent, CardHeader, CardTitle } from "../ui/card";
+import { Separator } from "../ui/separator";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "../ui/form";
+import { Input } from "../ui/input";
+import { Button } from "../ui/button";
+import { CalendarIcon, PlusCircle, Trash2 } from "lucide-react";
+import { Popover, PopoverContent, PopoverTrigger } from "../ui/popover";
+import { cn } from "@/lib/utils";
+import { format } from "date-fns";
+
+const educationSchema = z.object({
+  education: z.array(
+    z.object({
+      school: z.string().min(1, { message: "School is required." }),
+      degree: z.string().min(1, { message: "Degree is required." }),
+      graduationDate: z.object({
+        from: z.date({
+          required_error: "Please indicate the start of your education",
+        }),
+        to: z.date({ required_error: "Please indicate the graduation date" }),
+      }),
+    })
+  ),
+});
+
+export default function EducationForm() {
+  const form = useForm<z.infer<typeof educationSchema>>({
+    mode: "onSubmit",
+    shouldUnregister: false,
+    resolver: zodResolver(educationSchema),
+  });
+
+  const {
+    fields: educationFields,
+    append: addEducation,
+    remove: removeEducation,
+  } = useFieldArray({
+    control: form.control,
+    name: "education",
+  });
+
+  function onSubmit(values: z.infer<typeof educationSchema>) {
+    console.log(values);
+  }
+
+  return (
+    <Form {...form}>
+      <form onSubmit={form.handleSubmit(onSubmit)}>
+        <Card className="p-6">
+          <CardHeader>
+            <CardTitle>Education</CardTitle>
+          </CardHeader>
+          <CardContent className="space-y-6">
+            {educationFields.map((field, index) => (
+              <div key={field.id} className="space-y-4">
+                {index > 0 && <Separator className="my-6" />}
+                <FormField
+                  control={form.control}
+                  name={`education.${index}.school`}
+                  render={({ field }) => (
+                    <FormItem>
+                      <FormControl>
+                        <Input placeholder="School Name" {...field} />
+                      </FormControl>
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+                <FormField
+                  control={form.control}
+                  name={`education.${index}.degree`}
+                  render={({ field }) => (
+                    <FormItem>
+                      <FormControl>
+                        <Input placeholder="Degree" {...field} />
+                      </FormControl>
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+                <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
+                  <FormField
+                    control={form.control}
+                    name={`education.${index}.graduationDate.from`}
+                    render={({ field }) => (
+                      <FormItem className="flex flex-col">
+                        <FormLabel>From</FormLabel>
+                        <Popover>
+                          <PopoverTrigger asChild>
+                            <FormControl>
+                              <Button
+                                variant={"outline"}
+                                className={cn(
+                                  "w-full pl-3 text-left font-normal",
+                                  !field.value && "text-muted-foreground"
+                                )}
+                              >
+                                {field.value ? (
+                                  format(field.value, "LLL yyyy")
+                                ) : (
+                                  <span>Pick a date</span>
+                                )}
+                                <CalendarIcon className="ml-auto h-4 w-4 opacity-50" />
+                              </Button>
+                            </FormControl>
+                          </PopoverTrigger>
+                          <PopoverContent className="w-auto p-0" align="start">
+                            <Calendar
+                              mode="single"
+                              selected={field.value}
+                              onSelect={field.onChange}
+                              disabled={(date) =>
+                                date > new Date("2100-01-01") ||
+                                date < new Date("1900-01-01")
+                              }
+                              initialFocus
+                            />
+                          </PopoverContent>
+                        </Popover>
+                        <FormMessage />
+                      </FormItem>
+                    )}
+                  />
+                  <FormField
+                    control={form.control}
+                    name={`education.${index}.graduationDate.to`}
+                    render={({ field }) => (
+                      <FormItem className="flex flex-col">
+                        <FormLabel>To</FormLabel>
+                        <Popover>
+                          <PopoverTrigger asChild>
+                            <FormControl>
+                              <Button
+                                variant={"outline"}
+                                className={cn(
+                                  "w-full pl-3 text-left font-normal",
+                                  !field.value && "text-muted-foreground"
+                                )}
+                              >
+                                {field.value ? (
+                                  format(field.value, "LLL yyyy")
+                                ) : (
+                                  <span>Pick a date</span>
+                                )}
+                                <CalendarIcon className="ml-auto h-4 w-4 opacity-50" />
+                              </Button>
+                            </FormControl>
+                          </PopoverTrigger>
+                          <PopoverContent className="w-auto p-0" align="start">
+                            <Calendar
+                              mode="single"
+                              selected={field.value}
+                              onSelect={field.onChange}
+                              disabled={(date) =>
+                                date > new Date("2100-01-01") ||
+                                date < new Date("1900-01-01")
+                              }
+                              initialFocus
+                            />
+                          </PopoverContent>
+                        </Popover>
+                        <FormMessage />
+                      </FormItem>
+                    )}
+                  />
+                </div>
+
+                {index > 0 && (
+                  <Button
+                    variant="destructive"
+                    size="sm"
+                    onClick={() => removeEducation(index)}
+                  >
+                    <Trash2 className="w-4 h-4 mr-2" />
+                    Remove Education
+                  </Button>
+                )}
+              </div>
+            ))}
+            <Button
+              onClick={() =>
+                addEducation({
+                  school: "",
+                  degree: "",
+                  graduationDate: {
+                    from: new Date(),
+                    to: new Date(),
+                  },
+                })
+              }
+            >
+              <PlusCircle className="w-4 h-4 mr-2" />
+              Add Education
+            </Button>
+          </CardContent>
+        </Card>
+      </form>
+    </Form>
+  );
+}
